2ff35cb8168798ce876d13a3661def73e16d541c
Arnaud <acroisie@student.42lyon.fr>
2023-11-13 18:21:11 +0100

    WIP on detail view

---
+++ AirbnbClone/Components/ListingImageCarrousselView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingImageCarrousselView.swift
+//  AirbnbClone
+//
+//  Created by Arnaud CROISIER on 13/11/2023.
+//
+
+import SwiftUI
+
+struct ListingImageCarrousselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingImageCarrousselView()
+}
---
+++ AirbnbClone/Core/Listings/ListingDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingDetailView.swift
+//  AirbnbClone
+//
+//  Created by Arnaud CROISIER on 13/11/2023.
+//
+
+import SwiftUI
+
+struct ListingDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingDetailView()
+}
