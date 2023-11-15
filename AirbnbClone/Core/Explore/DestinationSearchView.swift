2fd30bb917799f4d52a3898e85b1b218503c36f6
Arnaud <acroisie@student.42lyon.fr>
2023-11-15 17:10:08 +0100

    Begin DestinationSearchView

---
+++ AirbnbClone/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationSearchView.swift
+//  AirbnbClone
+//
+//  Created by Arnaud CROISIER on 14/11/2023.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationSearchView()
+}
