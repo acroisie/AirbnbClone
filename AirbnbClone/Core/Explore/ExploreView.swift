21f1aadb77cd8405138fcc08908c35b47b902da4
Arnaud <acroisie@student.42lyon.fr>
2023-11-15 17:11:31 +0100

    Stage new previous changes

--- AirbnbClone/Core/Explore/ExploreView.swift
+++ AirbnbClone/Core/Explore/ExploreView.swift
@@ -8,22 +8,28 @@
 import SwiftUI
 
 struct ExploreView: View {
+    @State private var showDestinationSearchView: Bool = false
+    
     var body: some View {
         NavigationStack {
-            ScrollView {
-                SearchAndFilterBar()
+            if showDestinationSearchView {
+                
+            } else {
+                ScrollView {
+                    SearchAndFilterBar()
 
-                LazyVStack(spacing: 32) {
-                    ForEach(0 ..< 10, id: \.self) {listing in
-                        NavigationLink(value: listing) {
-                            ListingItemView()
+                    LazyVStack(spacing: 32) {
+                        ForEach(0 ..< 10, id: \.self) {listing in
+                            NavigationLink(value: listing) {
+                                ListingItemView()
+                            }
                         }
                     }
                 }
-            }
-            .navigationDestination(for: Int.self) { listing in
-                ListingDetailView()
-                    .navigationBarBackButtonHidden()
+                .navigationDestination(for: Int.self) { listing in
+                    ListingDetailView()
+                        .navigationBarBackButtonHidden()
+                }
             }
         }
     }
